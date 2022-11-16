3610f182e6b95ae758768ea015fb98aad146f8ea
joydunne <joydunne33@gmail.com>
2022-11-16 13:52:17 -0600

    new assignments

---
+++ ezGraph/stoppingInflow.py
@@ -0,0 +1,47 @@
+import numpy as np
+import time
+from ezGraph import * 
+from jStats import * 
+# Finite Difference Model
+
+#PARAMETERS
+dt = 1
+nsteps = 160
+
+r = 2.25 # radius (cm)
+Qin = 0 # Volume inflow rate (dV/dt) : (cubic cm/s)
+h = 50 #intial height (cm)
+k = 0.1 #outflow rate constant
+
+# EXPERIMENTAL DATA
+y_modeled = []
+
+# GRAPH
+graph = ezGraph (xmin=0, xmax=100,
+            xLabel= "Time (s)", 
+            yLabel= "Height (cm)")
+
+graph.add(0, h) # add intial vaules 
+
+# TIME LOOP
+for t in range (1, nsteps) :
+    modelTime = t * dt
+
+    if modelTime > 50:
+        Qin = 0 
+
+    #Filling
+    dh = Qin * dt / (np.pi * r **2) #find the change in height
+
+    h = h + dh #update height
+
+    # Draining
+    dVdt = -k * h
+    dh = dVdt * dt / (np.pi * r **2) #np.pi = pi 
+    h = h + dh
+
+    graph.add (modelTime , h)
+    #graph.wait (0.8)
+
+# DRAW GRAPH
+graph.keepOpen ()
